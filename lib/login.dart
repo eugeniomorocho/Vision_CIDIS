21228d36702ba2b0f0a9eea7d8a4a7c10e1e6eca
eugeniomorocho <sf4e@outlook.com>
2020-10-12 20:35:55 -0500

    Se termina la pantalla principal (boton login y boton sign up).

--- lib/login.dart
+++ lib/login.dart
@@ -110,28 +110,45 @@ class LoginPage extends StatelessWidget {
                     ),
 
                     //Botón attempt SIGN UP
-                    FlatButton(
-                      onPressed: () async {
-                        var username = _usernameController.text;
-                        var password = _passwordController.text;
-                        //Revisa en frontend que el username y password tengan mas de 4 caracteres
-                        if(username.length < 4)
-                          displayDialog(context, "Invalid Username", "The username should be at least 4 characters long");
-                        else if(password.length < 4)
-                          displayDialog(context, "Invalid Password", "The password should be at least 4 characters long");
-                        //Crea el usuario en backend o genera error
-                        else{
-                          var res = await attemptSignUp(username, password);
-                          if(res == 201)
-                            displayDialog(context, "Success", "The user was created. Log in now.");
-                          else if(res == 409)
-                            displayDialog(context, "That username is already registered", "Please try to sign up using another username, or log in if you already have an account.");
-                          else {
-                            displayDialog(context, "Error", "An unknown error occurred.");
-                          }
-                        }
-                      },
-                      child: Text("Sign Up"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account? ",
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+
+                        RawMaterialButton(
+                          constraints: BoxConstraints(),
+                          padding: EdgeInsets.all(5.0),
+                          onPressed: () async {
+                            var username = _usernameController.text;
+                            var password = _passwordController.text;
+                            //Revisa en frontend que el username y password tengan mas de 4 caracteres
+                            if(username.length < 4)
+                              displayDialog(context, "Invalid Username", "The username should be at least 4 characters long");
+                            else if(password.length < 4)
+                              displayDialog(context, "Invalid Password", "The password should be at least 4 characters long");
+                            //Crea el usuario en backend o genera error
+                            else{
+                              var res = await attemptSignUp(username, password);
+                              if(res == 201)
+                                displayDialog(context, "Success", "The user was created. Log in now.");
+                              else if(res == 409)
+                                displayDialog(context, "That username is already registered", "Please try to sign up using another username, or log in if you already have an account.");
+                              else {
+                                displayDialog(context, "Error", "An unknown error occurred.");
+                              }
+                            }
+                          },
+                          child: Text("Sign Up",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
 
                     //*********************************************************************
